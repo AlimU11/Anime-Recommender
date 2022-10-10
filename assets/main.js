bd35f18bd5530135dc003659230977c6c47a6ef8
AlimU11 <alim.uzhbanokov@gmail.com>
2022-10-10 14:36:14 +0300

    Fix double insertion

--- assets/main.js
+++ assets/main.js
@@ -5,12 +5,23 @@ function getMetaTag(name, content) {
     return metaTag;
 }
 
-document.getElementsByTagName('head')[0].appendChild(getMetaTag(
+// function that adds a meta tag to the head of the document if it doesn't already exist
+function addMetaTag(name, content) {
+    var metaTags = document.getElementsByTagName('meta');
+    for (var i = 0; i < metaTags.length; i++) {
+        if (metaTags[i].name === name) {
+            return;
+        }
+    }
+    document.getElementsByTagName('head')[0].appendChild(getMetaTag(name, content));
+}
+
+addMetaTag(
     "Description",
     "Anime recommender system for Anilist user profiles and individual title(s).",
-));
+);
 
-document.getElementsByTagName('head')[0].appendChild(getMetaTag(
+addMetaTag(
     "google-site-verification",
     "gld465HiecZa8jy-0b1iduTEp9Mg_hITVSqJpk35onQ",
-));
+);
